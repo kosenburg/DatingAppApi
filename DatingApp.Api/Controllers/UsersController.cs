a5bf553bf0494a0609f1fe671bce5a598d60dc00
kosenburg <kosenburg@gmail.com>
2018-12-13 19:09:45 -0500

    Initial Commit

---
+++ DatingApp.Api/Controllers/UsersController.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using AutoMapper;
+using DatingApp.Api.Data;
+using DatingApp.Api.Dtos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace DatingApp.Api.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController: ControllerBase
+    {
+        private readonly IDatingRepository _datingRepository;
+        private readonly IMapper _mapper;
+
+        public UsersController(IDatingRepository datingRepository, IMapper mapper)
+        {
+            _datingRepository = datingRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            var users = await _datingRepository.GetUsers();
+            var usersToReturn = _mapper.Map<IEnumerable<UserForListDto>>(users);
+            return Ok(usersToReturn);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUser(int id)
+        {
+            var user = await _datingRepository.GetUser(id);
+
+            var userToReturn = _mapper.Map<UserForDetailedDto>(user);
+            return Ok(userToReturn);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser(int id, UserForUpdateDto userForUpdateDto)
+        {
+            if (id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+            {
+                return Unauthorized();
+            }
+
+            var userFromRepo = await _datingRepository.GetUser(id);
+
+            _mapper.Map(userForUpdateDto, userFromRepo);
+
+            if (await _datingRepository.SaveAll())
+            {
+                return NoContent();
+            }
+            
+            throw new Exception($"Updating user with {id} failed on save.");
+        }
+    }
+}
---
+++ DatingApp.Api/Models/Value.cs
@@ -0,0 +1,8 @@
+namespace DatingApp.Api.Models
+{
+    public class Value
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+    }
+}
